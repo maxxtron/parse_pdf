c9b848c64c65acacda1c7f35450b483f43728be4
maxxtron <maxxtron93@gmail.com>
2024-05-07 09:17:29 +0300

    create

--- parse_pdf.js
+++ parse_pdf.js
@@ -3,7 +3,6 @@ let manuals = document.querySelectorAll('a[data-event="download-documentation"]'
 let name = document.querySelector('h1')
 function downloadPDF(array) {
 for (let url of array){
-const fileName = url?.querySelector('.c-downloads__title')?.textContent;
 let lastPart = url.href?.split("/")?.pop()?.replaceAll("%", "_");
 console.log(lastPart)
 const options = {
@@ -15,14 +14,10 @@ const options = {
     fetch(url)
     .then(response => response?.blob())
     .then(blob => {  
-        // Создаем ссылку для скачивания файла
         const url = window?.URL?.createObjectURL(new Blob([blob]));
-		console.log(url)
         const a = document?.createElement('a');
         a.href = url;
-        // a.download =fileName + ' ' + name.innerText + '.pdf'; // Указываем имя файла для скачивания
         a.download=lastPart
-        // Программно нажимаем на ссылку
         a.click();
         window?.URL?.revokeObjectURL(url);
     })

ba9760904f920daf944cb0e48b002031905ecf6b
maxxtron <maxxtron93@gmail.com>
2024-05-07 09:29:31 +0300

    add html

--- parse_pdf.js
+++ parse_pdf.js
@@ -1,29 +1,29 @@
+let manuals = ['https://winterwarm.compano.com/Data/Environments/000001/Attachment/Bijlage/PRD/ProductGroupBrochure/Brochure%20WCU%20EN.pdf', 'https://winterwarm.compano.com/Data/Environments/000001/Attachment/Bijlage/PRD/ProductGroupManual/Manual%20WCU_EN.pdf'];
 
-// let manuals = document.querySelectorAll('a[data-event="download-documentation"]');
-let manuals = ['https://winterwarm.compano.com/Data/Environments/000001/Attachment/Bijlage/PRD/ProductGroupBrochure/Brochure%20WCU%20EN.pdf','https://winterwarm.compano.com/Data/Environments/000001/Attachment/Bijlage/PRD/ProductGroupManual/Manual%20WCU_EN.pdf']
-let name = document.querySelector('h1')
 function downloadPDF(array) {
-    console.log(manuals)
-for (let url of array){
-let lastPart = url.href?.split("/")?.pop()?.replaceAll("%", "_");
-console.log(url)
-const options = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/pdf'
-        }
-    };
-    fetch(url)
-    .then(response => response?.blob())
-    .then(blob => {  
-        const url = window?.URL?.createObjectURL(new Blob([blob]));
-        const a = document?.createElement('a');
-        a.href = url;
-        a.download=lastPart
-        a.click();
-        window?.URL?.revokeObjectURL(url);
-    })
-    .catch(error => console.error('Ошибка при загрузке файла:', error));
+    for (let url of array) {
+        let lastPart = url.split("/").pop().replaceAll("%", "_");
+        console.log(url);
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/pdf'
+            }
+        };
+        fetch(url)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(new Blob([blob]));
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = lastPart;
+                a.click();
+                window.URL.revokeObjectURL(url);
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке файла:', error);
+            });
+    }
 }
-}
+
 downloadPDF(manuals);
